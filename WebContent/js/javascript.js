3d61a1171bde790e67062f52fb1121c02d708fbf
Leonardo <leonardocm92@hotmail.com>
2016-12-06 16:30:43 -0200

    Atualizações front

--- WebContent/js/javascript.js
+++ WebContent/js/javascript.js
@@ -1,16 +1,26 @@
 var g_cronometro;
-            var g_figuraAtual = 1;
-            function trocarImg(){
-               (g_figuraAtual < 2)?g_figuraAtual++:g_figuraAtual = 1;
-                document.getElementById("imgSlide").src="img/slider"+g_figuraAtual+".jpg";
-            }
-            function pause(){
-                clearInterval(g_cronometro);
-            }
-            function main(){
-                g_cronometro = setInterval(trocarImg,3000);
-            }
-			function play(){
-				clearInterval(g_cronometro);
-				g_cronometro = setInterval(trocarImg,3000);
-			}
+var g_figuraAtual = 1;
+function trocarImg(){
+   (g_figuraAtual < 2)?g_figuraAtual++:g_figuraAtual = 1;
+    document.getElementById("imgSlide").src="img/slider"+g_figuraAtual+".jpg";
+}
+function pause(){
+    clearInterval(g_cronometro);
+}
+function main(){
+    g_cronometro = setInterval(trocarImg,3000);
+}
+function play(){
+        clearInterval(g_cronometro);
+        g_cronometro = setInterval(trocarImg,3000);
+}
+ 
+ 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}                        
