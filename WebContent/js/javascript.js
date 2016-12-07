d8c76f46b30dac6c0d5a00a8de426b4e0873bf21
Leonardo TI <Leonardo TI@LeonardoTI>
2016-12-07 16:07:33 -0200

    Atividades

--- WebContent/js/javascript.js
+++ WebContent/js/javascript.js
@@ -1,19 +1,5 @@
 var g_cronometro;
 var g_figuraAtual = 1;
-function trocarImg(){
-   (g_figuraAtual < 2)?g_figuraAtual++:g_figuraAtual = 1;
-    document.getElementById("imgSlide").src="img/slider"+g_figuraAtual+".jpg";
-}
-function pause(){
-    clearInterval(g_cronometro);
-}
-function main(){
-    g_cronometro = setInterval(trocarImg,3000);
-}
-function play(){
-        clearInterval(g_cronometro);
-        g_cronometro = setInterval(trocarImg,3000);
-}
  
  
 function myFunction() {
@@ -23,4 +9,6 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
-}                        
+}              
+
+
