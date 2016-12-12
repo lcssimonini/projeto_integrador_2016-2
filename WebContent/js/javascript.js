3a7cb7cfda12c5b9d27b94131aa18cffe16bcd8e
Rafael Calixto <rafael.calixto@sankhya.com.br>
2016-12-12 19:04:43 -0200

    Implementacao do disparo do Email no formulário contato

--- WebContent/js/javascript.js
+++ WebContent/js/javascript.js
@@ -32,26 +32,49 @@ function showSlides() {
 
 
 function validar() {
-	campos=document.getElementsByTagName("input").length;
-	for(i=0;i<campos;i++){
+     campos=document.getElementsByTagName("input").length;
+     for(i=0;i<campos;i++){
         if(document.getElementsByTagName("input")[i].type=="text"){
-        	value = document.getElementsByTagName("input")[i].value;
-        	if(value=="" || value=="Campo Obrigatório*"){
-        		  document.getElementsByTagName("input")[i].value = "Campo Obrigatório*";
-        	}
+          value = document.getElementsByTagName("input")[i].value;
+          if(value=="" || value=="Campo Obrigatório*"){
+                 document.getElementsByTagName("input")[i].value = "Campo Obrigatório*";
+          }
         }
+     }    
+     var http = new XMLHttpRequest();
+     var url = "http://localhost:8080/sixfit/disparaEmail";
+     
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var feedback = document.getElementById("feedback").value;
+    
+     var params = "name="+name+"&email="+email+"&subject="+subject+"&feedback="+feedback;
+     http.open("POST", url, true);
 
-	}    
+     //Send the proper header information along with the request
+     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+     http.onreadystatechange = function() {//Call a function when the state changes.
+         if(http.readyState == 4 && http.status == 200) {
+             //alert(http.responseText);
+         }
+     }
+     http.send(params);
 }
 
+
+
+
+
 function limpar(idCampo) {
-	document.getElementById(idCampo).value = "";
+     document.getElementById(idCampo).value = "";
 }
 
 
 function newPopup(){
-	varWindow = window.open (
-	'cadastro.html',
-	'pagina',
-	"width=350, height=255, top=100, left=110, scrollbars=yes ,resizable=yes ,directories=yes , menubar=yes, toolbar=yes, titlebar=yes" );
+     varWindow = window.open (
+     'cadastro.html',
+     'pagina',
+     "width=350, height=255, top=100, left=110, scrollbars=yes ,resizable=yes ,directories=yes , menubar=yes, toolbar=yes, titlebar=yes" );
 }
